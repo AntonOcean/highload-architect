269ca8cb67519c38fb63b175fc0b9347dd85adb3
a.m.tsitulskiy <a.m.tsitulskiy@tinkoff.ru>
2023-02-26 16:58:54 +0300

    [HW-5] add sharding

--- chat/migrations/20230224173758_init-chat.up.sql
+++ chat/migrations/20230224173758_init-chat.up.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS chats
 (
-    id         UUID PRIMARY KEY,
+    id         UUID NOT NULL,
     sender_id UUID                     NOT NULL,
     receiver_id UUID                     NOT NULL,
     text text NOT NULL,
-    created    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+    created    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    PRIMARY KEY (id, sender_id)
 );
+
+CREATE INDEX IF NOT EXISTS chats_sender_receiver_created_idx ON  chats (sender_id, receiver_id, created DESC);
